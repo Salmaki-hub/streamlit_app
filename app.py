c91a07dbfbf7f370b90b8c10aef5a63f71b2e956
Malik Salman <82448827+Salmaki-hub@users.noreply.github.com>
2022-05-08 17:55:23 +0500

    upload files

---
+++ app.py
@@ -0,0 +1,147 @@
+import tensorflow as tf
+from tensorflow.keras import preprocessing 
+import streamlit as st
+import numpy as np
+from PIL import Image
+
+url = "https://github.com/streamlit/streamlit"
+st.set_page_config(page_title='Food Identification Tool', initial_sidebar_state = 'auto')
+st.title("Food Identification Tool")
+st.write("A machine learning powered system that tells accurately the food name. Check out code here [link](%s)." % url)
+
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)        
+
+file = st.sidebar.file_uploader("Upload Image", type=['jpeg','jpg','png'])
+
+
+cat = ['apple pie',
+         'baby back ribs',
+         'baklava',
+         'beef carpaccio',
+         'beef tartare',
+         'beet salad',
+         'beignets',
+         'bibimbap',
+         'bread pudding',
+         'breakfast burrito',
+         'bruschetta',
+         'caesar salad',
+         'cannoli',
+         'caprese salad',
+         'carrot cake',
+         'ceviche',
+         'cheese plate',
+         'cheesecake',
+         'chicken curry',
+         'chicken quesadilla',
+         'chicken wings',
+         'chocolate cake',
+         'chocolate mousse',
+         'churros',
+         'clam chowder',
+         'club sandwich',
+         'crab cakes',
+         'creme brulee',
+         'croque madame',
+         'cup cakes',
+         'deviled eggs',
+         'donuts',
+         'dumplings',
+         'edamame',
+         'eggs benedict',
+         'escargots',
+         'falafel',
+         'filet mignon',
+         'fish and_chips',
+         'foie gras',
+         'french fries',
+         'french onion soup',
+         'french toast',
+         'fried calamari',
+         'fried rice',
+         'frozen yogurt',
+         'garlic bread',
+         'gnocchi',
+         'greek salad',
+         'grilled cheese sandwich',
+         'grilled salmon',
+         'guacamole',
+         'gyoza',
+         'hamburger',
+         'hot and sour soup',
+         'hot dog',
+         'huevos rancheros',
+         'hummus',
+         'ice cream',
+         'lasagna',
+         'lobster bisque',
+         'lobster roll sandwich',
+         'macaroni and cheese',
+         'macarons',
+         'miso soup',
+         'mussels',
+         'nachos',
+         'omelette',
+         'onion rings',
+         'oysters',
+         'pad thai',
+         'paella',
+         'pancakes',
+         'panna cotta',
+         'peking duck',
+         'pho',
+         'pizza',
+         'pork chop',
+         'poutine',
+         'prime rib',
+         'pulled pork sandwich',
+         'ramen',
+         'ravioli',
+         'red velvet cake',
+         'risotto',
+         'samosa',
+         'sashimi',
+         'scallops',
+         'seaweed salad',
+         'shrimp and grits',
+         'spaghetti bolognese',
+         'spaghetti carbonara',
+         'spring rolls',
+         'steak',
+         'strawberry shortcake',
+         'sushi',
+         'tacos',
+         'octopus balls',
+         'tiramisu',
+         'tuna tartare',
+         'waffles']
+
+def prediction(image, model):
+    test_image = image.resize((224,224))
+    test_image = preprocessing.image.img_to_array(test_image)
+    test_image = test_image / 255.0
+    test_image = np.expand_dims(test_image, axis=0)
+    result=model.predict(test_image)
+    result=np.argmax(result)
+    Pred=cat[result]
+    return Pred
+
+
+
+if file is not None:
+    img = Image.open(file)
+    model = tf.keras.models.load_model("my_model.h5")
+    img_jpeg = img.convert('RGB')
+    pred = prediction(img_jpeg, model)
+    #score = tf.nn.softmax(prediction[0])
+    st.markdown(f"<h2 style='text-align: center; color: black;'>{pred}</h2>", unsafe_allow_html=True)
+    st.image(img, use_column_width=True)
+    
